55aedb9ce60f23582c74c8a1ca22a239b20bf3b5
Huang Jian-Yu <bubu19861213@gmail.com>
2015-07-20 21:36:03 +0800

    Initial Commit

---
+++ SideoutMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideoutMenu
+//
+//  Created by Huang Jian-Yu on 2015/7/20.
+//  Copyright (c) 2015年 Zitra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
